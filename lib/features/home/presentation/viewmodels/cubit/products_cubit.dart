6ce2863740d801c2f38a5d372e8c1fdf9b5bdd2e
MoHa270xX <mhmdmedo102030@gmail.com>
2025-09-21 16:28:37 +0300

    create products  cubit method

--- lib/features/home/presentation/viewmodels/cubit/products_cubit.dart
+++ lib/features/home/presentation/viewmodels/cubit/products_cubit.dart
@@ -1,9 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:fluttertask/features/home/data/models/product_model/product_model.dart';
+import 'package:fluttertask/features/home/data/repos/home_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'products_state.dart';
 
 class ProductsCubit extends Cubit<ProductsState> {
-  ProductsCubit() : super(ProductsInitial());
+  ProductsCubit(this.homeRepo) : super(ProductsInitial());
+  final HomeRepo homeRepo;
+  Future<void> fetchAllProducts()async {
+    emit(ProductsLoading());
+    var res = await homeRepo.fethAllProducts();
+    res.fold(  (Failure) {
+        emit(ProductsFailure(Failure.errMessage));
+      },
+      (products) {
+        emit(ProductsSuccess(products));
+      },
+    );
+  }
 }
