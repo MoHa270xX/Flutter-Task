bdf117570cdef353cd231b60ab6429c3aa1bebf0
MoHa270xX <mhmdmedo102030@gmail.com>
2025-09-21 16:10:23 +0300

    create home repo implementation

--- lib/core/utils/api_services.dart
+++ lib/core/utils/api_services.dart
@@ -4,7 +4,7 @@ class ApiService {
   final _baseUrl = "https://fakestoreapi.com/products";
   final Dio dio;
   ApiService(this.dio);
-  Future<Map<dynamic, String>> getdata() async {
+  Future<List<dynamic>> getdata() async {
     var response = await dio.get(_baseUrl);
     return response.data;
   }
---
+++ lib/features/home/data/repos/home_repo_imple.dart
@@ -0,0 +1,24 @@
+import 'package:dartz/dartz.dart';
+import 'package:fluttertask/core/errors/failure.dart';
+import 'package:fluttertask/core/utils/api_services.dart';
+import 'package:fluttertask/features/home/data/models/product_model/product_model.dart';
+import 'package:fluttertask/features/home/data/repos/home_repo.dart';
+
+class HomeRepoImple implements HomeRepo {
+  final ApiService apiService;
+  HomeRepoImple(this.apiService);
+
+  @override
+  Future<Either<Failure, List<ProductModel>>> fethAllProducts() async {
+    try {
+      var data = await apiService.getdata();
+      List<ProductModel> products = [];
+      for (int i = 0; i < data.length; i++) {
+        products.add(ProductModel.fromJson(data[i]));
+      }
+      return Right(products);
+    } catch (e) {
+      return left(ServerFailure());
+    }
+  }
+}
