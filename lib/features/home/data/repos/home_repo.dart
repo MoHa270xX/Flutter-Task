abe4b246f3e612e5cb21b175c06899c6734b4b9b
MoHa270xX <mhmdmedo102030@gmail.com>
2025-09-21 15:37:48 +0300

    create home repo

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:fluttertask/core/errors/failure.dart';
+import 'package:fluttertask/features/home/data/models/product_model/product_model.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure,List<ProductModel>>> fethAllProducts();
+}
